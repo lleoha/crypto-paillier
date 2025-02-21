86ca93aed71400d8fb461b1d2f870989ba19d9c0
Mateusz Kramarczyk <kramarczyk@gmail.com>
2025-02-21 11:52:33 +0100

    Update public interface.

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,17 @@ use crypto_bigint::{U1024, U1536, U2048, U3072, U4096, U6144, U8192};
 mod pk;
 mod sk;
 
+pub use pk::PublicKey as PaillierPublicKey;
+pub use sk::SecretKey as PaillierSecretKey;
+
 pub type PaillierSecretKey2048 =
-    sk::SecretKey<{ U1024::LIMBS }, { U2048::LIMBS }, { U4096::LIMBS }>;
-pub type PaillierPublicKey2048 = pk::PublicKey<{ U2048::LIMBS }, { U4096::LIMBS }>;
+    PaillierSecretKey<{ U1024::LIMBS }, { U2048::LIMBS }, { U4096::LIMBS }>;
+pub type PaillierPublicKey2048 = PaillierPublicKey<{ U2048::LIMBS }, { U4096::LIMBS }>;
 
 pub type PaillierSecretKey3072 =
-    sk::SecretKey<{ U1536::LIMBS }, { U3072::LIMBS }, { U6144::LIMBS }>;
-pub type PaillierPublicKey3072 = pk::PublicKey<{ U3072::LIMBS }, { U6144::LIMBS }>;
+    PaillierSecretKey<{ U1536::LIMBS }, { U3072::LIMBS }, { U6144::LIMBS }>;
+pub type PaillierPublicKey3072 = PaillierPublicKey<{ U3072::LIMBS }, { U6144::LIMBS }>;
 
 pub type PaillierSecretKey4096 =
-    sk::SecretKey<{ U2048::LIMBS }, { U4096::LIMBS }, { U8192::LIMBS }>;
-pub type PaillierPublicKey4096 = pk::PublicKey<{ U4096::LIMBS }, { U8192::LIMBS }>;
+    PaillierSecretKey<{ U2048::LIMBS }, { U4096::LIMBS }, { U8192::LIMBS }>;
+pub type PaillierPublicKey4096 = PaillierPublicKey<{ U4096::LIMBS }, { U8192::LIMBS }>;
